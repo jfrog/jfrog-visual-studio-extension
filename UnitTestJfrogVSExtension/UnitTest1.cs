6f0af9681f014d4b3912113d4a7e0f3c127bf558
kerenr-jfrog <kerenr@jfrog.com>
2024-09-12 14:51:56 +0300

    Update tests.yml

--- UnitTestJfrogVSExtension/UnitTest1.cs
+++ UnitTestJfrogVSExtension/UnitTest1.cs
@@ -1,14 +1,18 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using System.Diagnostics;
 
 namespace UnitTestJfrogVSExtension
 {
     [TestClass]
     public class UnitTest1
     {
+        public TestContext TestContext { get; set; }
+
         [TestMethod]
         public void TestMethod1()
         {
+            TestContext.WriteLine("Success!!!");
         }
     }
 }
