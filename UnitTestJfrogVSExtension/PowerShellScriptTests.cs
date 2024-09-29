2255813b235a35ca327a174c07f036b7ee76523e
kerenr-jfrog <kerenr@jfrog.com>
2024-09-29 09:48:33 +0300

    vsix added to release + published to vs marketplace + add unitests for ps scripts

---
+++ UnitTestJfrogVSExtension/PowerShellScriptTests.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.IO;
+using System.Diagnostics;
+using System;
+
+namespace UnitTestJfrogVSExtension
+{
+    [TestClass]
+    public class PowerShellScriptTests
+    {
+        public TestContext TestContext { get; set; }
+
+        // use relative path (from base dir) in order to be able to run in other environments
+        public static string baseDir = AppDomain.CurrentDomain.BaseDirectory;
+        public static string scriptPath = Path.GetFullPath(Path.Combine(baseDir, @"..\..\..\scripts\UpdateVsixVersion.ps1"));
+
+        [TestMethod]
+        public void Test_UpdateVsixVersion_ValidVersion()
+        {
+            var envVars = new Dictionary<string, string>
+            {
+                { "NEW_VERSION", "vs22-1.2.3" }, // Valid version
+            };
+
+            // script should succeed and return exit code 0
+            int exitCode = RunPowerShellScript(scriptPath, envVars);
+            Assert.AreEqual(0, exitCode);
+        }
+
+        [TestMethod]
+        public void Test_UpdateVsixVersion_InvalidVersion()
+        {
+            var envVars = new Dictionary<string, string>
+            {
+                { "NEW_VERSION", "vs-1.5.56" } // Invalid version
+            };
+
+            // script should fail and return exit code 1
+            int exitCode = RunPowerShellScript(scriptPath, envVars);
+            Assert.AreEqual(1, exitCode);
+        }
+
+        private int RunPowerShellScript(string scriptPath, Dictionary<string, string> envVars)
+        {
+            // Create a new process to run the PowerShell script
+            ProcessStartInfo processInfo = new ProcessStartInfo("powershell.exe", $"-ExecutionPolicy Bypass -File \"{scriptPath}\"")
+            {
+                RedirectStandardOutput = true,
+                RedirectStandardError = true,
+                UseShellExecute = false,
+                CreateNoWindow = true
+            };
+
+            // Set environment variables
+            foreach (var envVar in envVars)
+            {
+                processInfo.EnvironmentVariables[envVar.Key] = envVar.Value; 
+            }
+
+            // Run the script in a process
+            using (Process process = new Process())
+            {
+                process.StartInfo = processInfo;
+                process.Start();
+                process.WaitForExit();
+
+                // read output and error
+                string output = process.StandardOutput.ReadToEnd();
+                string error = process.StandardError.ReadToEnd();
+
+                // log output and error for debug
+                Debug.WriteLine($"Output: {output}");
+                Debug.WriteLine($"Error: {error}");
+
+                return process.ExitCode;
+            }
+
+        }
+
+    }
+}
--- scripts/UpdateVsixVersion.ps1
+++ scripts/UpdateVsixVersion.ps1
@@ -1,7 +1,16 @@
 $newVersion = $env:NEW_VERSION
 $manifestFileLocation = "JfrogVSExtension/source.extension.vsixmanifest"
 
-$newVersion = $newVersion.Substring(1) # Extract version from the tag (e.g., 'v1.0.0' -> '1.0.0')
+# Validate the NEW_VERSION format: "vs22-x.x.x"
+if ($newVersion -match "^vs22-(\d+\.\d+\.\d+)$") {
+    # Extract version from the tag (e.g., 'vs22-1.0.0' -> '1.0.0')
+    $newVersion = $matches[1] 
+} else {
+     Write-Error "Error: NEW_VERSION must be in the format 'vs22-x.x.x' where x is an integer."
+    exit 1
+}
+
+# Load the manifest file and update the version
 [xml]$manifest = Get-Content -Path $manifestFileLocation
 $manifest.PackageManifest.Metadata.Identity.Version = $newVersion
 $manifest.Save($manifestFileLocation)
