0916c1c9e970ec0bbf5766fdfc4d84e38f79ea4d
kerenr-jfrog <kerenr@jfrog.com>
2024-11-06 17:52:33 +0200

    added unitest

--- scripts/ValidatePDBFilesAbsenceInReleaseMode.ps1
+++ scripts/ValidatePDBFilesAbsenceInReleaseMode.ps1
@@ -1,11 +1,14 @@
-# Define the path to the VSIX file - after building the project in Release mode
-$vsixPath = "JfrogVSExtension\bin\Release\JfrogVSExtension.vsix"
+# if vsix file path was not defined - use the default location in release folder
+if (-not $env:VSIX_PATH) {
+    $env:VSIX_PATH=Join-Path "$(pwd)" "JfrogVSExtension\bin\Release\JfrogVSExtension.vsix"
+}
+
 $pdbExists = $false
 $fileName = ""
         
 # Check if the .vsix file contains the PDB file
-if (Test-Path $vsixPath) {
-    $zipContent = [System.IO.Compression.ZipFile]::OpenRead($vsixPath)
+if (Test-Path $env:VSIX_PATH) {
+    $zipContent = [System.IO.Compression.ZipFile]::OpenRead($env:VSIX_PATH)
     foreach ($entry in $zipContent.Entries) {
         if ($entry.FullName -like "*JfrogVSExtension.pdb") {
             $pdbExists = $true
@@ -15,7 +18,7 @@ if (Test-Path $vsixPath) {
     }
     $zipContent.Dispose()
 } else {
-    Write-Error "VSIX file does not exist in the following path: $vsixPath."
+    Write-Error "VSIX file does not exist in the following path: $env:VSIX_PATH."
     exit 1 # Fail the workflow if the .vsix file is not found
 }
 
@@ -23,5 +26,5 @@ if ($pdbExists) {
     Write-Error  "PDB file exists in VSIX. file name = $fileName Please check your project settings."
     exit 1  # Fail the workflow if the PDB file is found
 } else {
-    Write-Host "PDB file not found in VSIX. Build is clean."
+    Write-Host "PDB file not found in VSIX. Release mode build is clean."
 }
